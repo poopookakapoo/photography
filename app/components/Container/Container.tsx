6583bcabf542bdff23daf3e90befc3dd19156ec2
Sean Terzi <seanterzi@poopoo.local>
2026-01-15 05:42:09 +0100

    Move app to src/app + remove old root app folder

--- app/components/Container/Container.tsx
+++
@@ -1,5 +0,0 @@
-import styles from "./Container.module.css";
-
-export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className={styles.container}>{children}</div>;
-}
--- app/components/Effects/Noise.tsx
+++
@@ -1,3 +0,0 @@
-export default function Noise() {
-  return <div aria-hidden className="__noise" />;
-}
